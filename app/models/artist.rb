8a6433e5802c043ce6d21d2de5c9013431fb2512
github username <github email address>
2019-09-25 08:15:53 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,10 +1,46 @@
 class Artist
-
+  @@all = [] 
   attr_reader :name, :years_experience
-
+  
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
+
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def paintings
+    Painting.all.select do |painting| 
+      painting.artist == self
+    end
+  end
+
+  def galleries 
+    paintings.map(&:gallery)
+  end
+
+  def cities 
+    galleries.map(&:city)
+  end
+
+  def create_painting(title, price,gallery)
+    Painting.new(title, price, self, gallery)
+  end
+
+  def self.total_experience
+    self.all.reduce (0) do |sum, artist| 
+      sum + artist.years_experience
+    end
+  end
+
+  def self.most_prolific
+    self.all.max_by do |artist|
+      artist.paintings.count.to_f / artist.years_experience
+    end
   end
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,8 +1,34 @@
 require_relative '../config/environment.rb'
 
 
+artist1 = Artist.new('vincent', 6720)
+artist2 = Artist.new('bubbles', 2)
+artist3 = Artist.new('leo', 7)
+artist4 = Artist.new('john', 1)
 
 
-binding.pry
+gallery1 = Gallery.new('nice place', 'new york')
+gallery2 = Gallery.new('fancy place', 'paris')
+gallery3 = Gallery.new('meh', 'denver')
+gallery4 = Gallery.new('jazzy town', 'dayton ohio')
+
+painting1 = Painting.new('a duck', 50, artist1, gallery1)
+painting2 = Painting.new('a duck on a boat', 350, artist1, gallery2)
+painting3 = Painting.new('a boat on sea of ducks', 500, artist2, gallery3)
+painting4 = Painting.new('a ship', 5030, artist3, gallery3)
+
+# p painting1
+# p painting2
+# p painting3 
+# p painting4
+
+p artist1.paintings.count
+p artist1.galleries
+p artist1.cities
+p Artist.most_prolific
+p Painting.total_price
+
+p gallery3.most_expensive_painting
+# binding.pry
 
 puts "Bob Ross rules."
